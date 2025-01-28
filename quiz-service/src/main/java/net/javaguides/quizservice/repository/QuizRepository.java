b688a25a555add6dc940e40a2cfb78de4ee236cd
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-01-28 01:51:00 -0600

    Add quiz controller

--- quiz-service/src/main/java/net/javaguides/quizservice/repository/QuizRepository.java
+++ quiz-service/src/main/java/net/javaguides/quizservice/repository/QuizRepository.java
@@ -3,5 +3,5 @@ package net.javaguides.quizservice.repository;
 import net.javaguides.quizservice.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
+public interface QuizRepository extends JpaRepository<Quiz, Integer> {
 }
